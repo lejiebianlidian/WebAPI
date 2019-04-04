61894e4be8c317a8bd7f6d8781913b324ca57173
lejiebianlidian <visualstudio365@gmail.com>
2019-04-04 17:54:16 +0800

    修改原有代码，测试api

--- APITest/Controllers/ValuesController.cs
+++ APITest/Controllers/ValuesController.cs
@@ -9,31 +9,63 @@ namespace APITest.Controllers
 {
     public class ValuesController : ApiController
     {
+        /// <summary>
+        /// 手动添加代码
+        /// </summary>
+        private List<string> mockDataList = new List<string>()
+        {
+
+            "Product A",
+            "Product B",
+            "Product C",
+            "Product D",
+            "Product E"
+
+        };
+
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return mockDataList;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            //return "value";
+            return mockDataList[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            mockDataList.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            mockDataList[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            mockDataList.RemoveAt(id);
         }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
